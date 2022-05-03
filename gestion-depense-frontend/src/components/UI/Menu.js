2c0710e9d2c0153585dc53cd3a94268858279159
Potrunks <82607603+Potrunks@users.noreply.github.com>
2022-05-04 00:44:43 +0200

    Add button "new spending period"

--- gestion-depense-frontend/src/components/UI/Menu.js
+++ gestion-depense-frontend/src/components/UI/Menu.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const Menu = () => {
+  const { idUserConnected } = useParams();
   return (
-    <div>Menu</div>
-  )
-}
+    <div className="app-main-container">
+      <div className="main-form-container">
+        <div className="main-title-form">
+          <span>Menu</span>
+        </div>
+        <div className="main-button-container">
+          <button>New spending period</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
