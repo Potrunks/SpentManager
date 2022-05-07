f15dc5e200be99f726699ccca2e9ea1a03a3bdbf
Potrunks <82607603+Potrunks@users.noreply.github.com>
2022-05-08 01:09:49 +0200

    Create a success page

--- gestion-depense-frontend/src/components/page/Success.js
+++ gestion-depense-frontend/src/components/page/Success.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import successImage from "./success.png";
 
 const Success = () => {
-  return (
-    <div>Success</div>
-  )
-}
 
-export default Success
+  const navigate = useNavigate();
+
+  return (
+    <div className="app-main-container">
+      <div className="page-container">
+        <div className="title-container">
+          <span>Success</span>
+        </div>
+        <div className="text-container">
+          <span>Your action have been save successfully</span>
+        </div>
+        <div className="img-container">
+          <img src={successImage} alt="Success"></img>
+        </div>
+        <div className="main-button-container">
+          <button onClick={() => {
+            navigate("/menu")
+          }}>OK</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
