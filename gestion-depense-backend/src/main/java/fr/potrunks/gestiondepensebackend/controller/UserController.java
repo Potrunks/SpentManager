6847ef2432e6e2ffe3dc62ceac58741ba6371e7e
Potrunks <82607603+Potrunks@users.noreply.github.com>
2022-05-10 16:31:55 +0200

    Create user controller and method to fetch all user in period spent in progress

---
+++ gestion-depense-backend/src/main/java/fr/potrunks/gestiondepensebackend/controller/UserController.java
@@ -0,0 +1,27 @@
+package fr.potrunks.gestiondepensebackend.controller;
+
+import fr.potrunks.gestiondepensebackend.business.UserIBusiness;
+import fr.potrunks.gestiondepensebackend.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/spentmanager/user/")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserIBusiness userIBusiness;
+
+    @GetMapping("/getusersbyperiodspentinprogress")
+    public List<User> fetchUsersByPeriodSpentInProgress() {
+        return userIBusiness.getAllByPeriodSpentInProgress();
+    }
+}
